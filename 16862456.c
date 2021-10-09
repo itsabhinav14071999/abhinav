12bde4657517e86d62945da0e8bf45918d2189e0
vanihba14 <42239125+vanihba14@users.noreply.github.com>
2021-10-09 12:17:26 +0530

    Update 16862456.c

--- 16862456.c
+++ 16862456.c
@@ -11,11 +11,13 @@ int main()
         scanf("%d%d",&a,&b);
         if(a<b)
         {
-           t=5;
-            t++;
-            if(t==0){
-                return 0;    
-            }
+            a=a+b;
+            b=a-b;
+            a=a-b;
+            a=a+b;
+            b=a-b;
+            a=a-b;
+           
             a=a+b;
             b=a-b;
             a=a-b;
